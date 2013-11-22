6a937ffada09b8594050200bd0a63597746bfb97
Louis Bataillard <info@mobweb.ch>
2013-11-22 19:18:10 +0100

    removed some references to boilerplate extension

--- app/code/community/MobWeb/BrandTurnover/Helper/Data.php
+++ app/code/community/MobWeb/BrandTurnover/Helper/Data.php
@@ -5,7 +5,6 @@ class MobWeb_BrandTurnover_Helper_Data extends Mage_Core_Helper_Abstract
 	public $log_file = 'mobweb_brandturnover.log';
 
 	// Returns an array with the brand IDs and the turnover limit specified
-	// to receive a coupon for this brand
 	public function getTurnoverLimits()
 	{
 		// Get the brand specific turnover for each brand as specified
